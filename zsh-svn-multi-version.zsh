9860080e857a9f10d807e8416d92d97cc862a371
peaceman <nixalio@gmail.com>
2013-11-26 00:17:57 +0100

    use a definable binary list
    let the user define which svn binaries should be used, instead of
    hardcoding paths.
    
    the binaries to use can be defined in the SVN_BINARIES variable

--- zsh-svn-multi-version.zsh
+++ zsh-svn-multi-version.zsh
@@ -1,24 +1,29 @@
 # vim:ft=zsh ts=2 sw=2 sts=2
 #
-local svn_old=/usr/bin/svn
-local svn_new=/usr/local/bin/svn
-local svn_cli=$svn_new
+local msg='You have to define a list of available svn binaries ($SVN_BINARIES) for zsh-svn-multi-version'
+if (( ! $+SVN_BINARIES )); then
+    echo $msg
+    return 1
+fi
 
 alias svn=_svn_wrapper
 
 function _svn_wrapper() {
-    svn_to_use=$svn_cli
+    local current_svn_binary_index=1
+    local svn_to_use=$SVN_BINARIES[$current_svn_binary_index]
     
-    if _current_dir_is_svn_repo; then
-        if _has_old_repo_format; then
-            svn_to_use=$svn_old
-        fi
+    if _current_dir_is_svn_repo $svn_to_use; then
+        while _has_old_repo_format $svn_to_use; do
+            let "current_svn_binary_index++"
+            svn_to_use=$SVN_BINARIES[$current_svn_binary_index]
+        done
     fi
     
     $svn_to_use "$@"
 }
 
 function _has_old_repo_format() {
+    local svn_cli=$1
     if $($svn_cli info 2>&1 | grep E155036 > /dev/null); then
         return 0
     fi
@@ -26,6 +31,7 @@ function _has_old_repo_format() {
 }
 
 function _current_dir_is_svn_repo() {
+    local svn_cli=$1
     if $($svn_cli info 2>&1 | grep E155007 > /dev/null); then
         return 1
     fi
